4c3d93b89674bcc1629649fa1965fd72bbc14e88
Maegan Wilson <maegan@maeganwilson.com>
2022-02-01 21:58:12 -0600

    Pops up from bottom

--- Sources/CoffeeToast/CoffeeToast.swift
+++ Sources/CoffeeToast/CoffeeToast.swift
@@ -5,3 +5,38 @@ public struct CoffeeToast {
 
     public init() {}
 }
+
+public struct Toast: View {
+    @Binding var isShown: Bool
+
+    var color: Color
+    var text: String
+    var duration: Double
+    var foregroundColor: Color
+
+    public init(isShown: Binding<Bool>, color: Color, text: String, duration: Double, foregroundColor: Color = .white) {
+        self._isShown = isShown
+        self.color = color
+        self.text = text
+        self.duration = duration
+        self.foregroundColor = foregroundColor
+    }
+
+    public var body: some View {
+        VStack {
+            Spacer()
+            Text("Hello")
+                .padding()
+                .background(color)
+                .foregroundColor(foregroundColor)
+                .cornerRadius(10.0)
+                .offset(x: 0, y: isShown ? -10 : 100)
+                .animation(.default, value: isShown)
+                .onChange(of: isShown) { newValue in
+                    DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                        self.isShown = false
+                    }
+                }
+        }
+    }
+}
