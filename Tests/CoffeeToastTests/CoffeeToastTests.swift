a0e9da5b90944d5d30532ba573589cec8524cf61
Thimo Bess <thimo.bess@gmail.com>
2022-02-03 12:27:39 +0100

    Remove not needed code

--- Tests/CoffeeToastTests/CoffeeToastTests.swift
+++ Tests/CoffeeToastTests/CoffeeToastTests.swift
@@ -5,12 +5,6 @@ import Combine
 @testable import CoffeeToast
 
 final class CoffeeToastTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CoffeeToast().text, "Hello, World!")
-    }
 
     func testToast() throws {
         let sampleToast = Toast(isShown: Binding.constant(true), color: .red, text: "Error", duration: 2.0)
