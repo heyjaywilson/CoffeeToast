9dbfe095eed223f68e0790ef995ddbc9c4c35a3b
Thimo Bess <thimo.bess@gmail.com>
2022-02-03 12:28:01 +0100

    Add content view to Toast

--- Sources/CoffeeToast/CoffeeToast.swift
+++ Sources/CoffeeToast/CoffeeToast.swift
@@ -6,41 +6,57 @@ import SwiftUI
 /// - Parameter text: Text the notification is supposed to show
 /// - Parameter duration: How long is the notification supposed to show before dismissing
 /// - Parameter foregroundColor: The color of the text. This is optional and defaults to `Color.white`
-public struct Toast: View {
+/// - Parameter content: The view which will present the toast
+public struct Toast<Content: View>: View {
     @Binding var isShown: Bool
 
-    var color: Color
     var text: String
     var duration: Double
     var foregroundColor: Color
+    var backgroundColor: Color
 
-    public init(isShown: Binding<Bool>,
-                color: Color,
-                text: String,
-                duration: Double,
-                foregroundColor: Color = .white) {
+    private let content: Content
+
+    public init(_ text: String,
+                duration: Double = 2.0,
+                foregroundColor: Color = .white,
+                backgroundColor: Color = .red,
+                isShown: Binding<Bool>,
+                @ViewBuilder content: () -> Content) {
         self._isShown = isShown
-        self.color = color
         self.text = text
         self.duration = duration
         self.foregroundColor = foregroundColor
+        self.backgroundColor = backgroundColor
+        self.content = content()
     }
 
     public var body: some View {
-        VStack {
-            Spacer()
-            Text("Hello")
-                .padding()
-                .background(color)
-                .foregroundColor(foregroundColor)
-                .cornerRadius(10.0)
-                .offset(x: 0, y: isShown ? -10 : 100)
-                .animation(.default, value: isShown)
-                .onChange(of: isShown) { newValue in
-                    DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                        self.isShown = false
+        ZStack {
+            content
+            VStack {
+                Spacer()
+                Text(text)
+                    .padding()
+                    .background(backgroundColor)
+                    .foregroundColor(foregroundColor)
+                    .cornerRadius(10.0)
+                    .offset(x: 0, y: isShown ? -10 : 100)
+                    .animation(.default, value: isShown)
+                    .onChange(of: isShown) { newValue in
+                        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                            self.isShown = false
+                        }
                     }
-                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Toast("TOAST NOTIFICATION", isShown: .constant(true)) {
+            Text("This is my awesome content")
         }
     }
 }
